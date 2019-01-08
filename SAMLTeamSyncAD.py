425a3911e3d59a4f3838a48c4c3380acee12a59d
Jared Murrell <primetheus@github.com>
2019-01-08 16:13:10 -0500

    migrated to ldap3 for pagination
    moved from python-ldap to python-ldap3 for better feature support

--- SAMLTeamSyncAD.py
+++ SAMLTeamSyncAD.py
@@ -1,8 +1,7 @@
 import sys
-import ldap
 import yaml
 import argparse
-from pprint import pprint
+from ldap3 import Server, Connection, ALL
 from github import Github, GithubException
 if (sys.version_info > (3, 0)):
     from urllib.parse import urlparse
@@ -18,6 +17,7 @@ class ADSync:
             self.GITHUB_TOKEN = settings['github']['token']
             self.AD_SERVERS = settings['ldap']['servers']
             self.AD_SERVER_PORT = settings['ldap']['port']
+            self.AD_BASEDN = settings['ldap']['base_dn']
             self.AD_USER_BASEDN = settings['ldap']['user_base_dn']
             self.AD_GROUP_BASEDN = settings['ldap']['group_base_dn']
             self.AD_USER_FILTER = settings['ldap']['user_filter']
@@ -25,114 +25,41 @@ class ADSync:
             self.AD_GROUP_FILTER = settings['ldap']['group_filter']
             self.AD_BIND_USER = settings['ldap']['bind_user']
             self.AD_BIND_PWD = settings['ldap']['bind_password']
-            self.AD_SEARCH_DN = settings['ldap']['search_dn']
+            self.AD_PAGE_SIZE = settings['ldap']['page_size']
+            #self.AD_SEARCH_DN = settings['ldap']['search_dn']
             self.SERVER = urlparse(self.GITHUB_SERVER)
 
-    def ad_auth(self, server=None, username=None, password=None, port=None, start_tls=False):
-        """
-        Connect to Active Directory
-        :param username: Username of account to search with
-        :param password: Password of the account to search with
-        :param server: IP Address or hostname of the domain controller
-        :param port: LDAP port to connect to
-        :param start_tls: Whether or not to start TLS
-        :return:
-        """
-        server = self.AD_SERVERS[0] if not server else server
-        username = self.AD_BIND_USER if not username else username
-        password = self.AD_BIND_PWD if not password else password
-        port = self.AD_SERVER_PORT if not port else port
-        prefix = 'ldaps' if start_tls else 'ldap'
-        conn = ldap.initialize('{}://{}:{}'.format(prefix, server, port))
-        conn.protocol_version = 3
-        conn.set_option(ldap.OPT_REFERRALS, 0)
-        result = True
-        try:
-            conn.simple_bind_s(username, password)
-            print("Succesfully authenticated to {}".format(server))
-        except ldap.INVALID_CREDENTIALS:
-            print("Invalid Active Directory credentials")
-            return "Invalid credentials", False
-        except ldap.SERVER_DOWN:
-            print("{}: Server is unreachable".format(server))
-            return "{}: Server is unreachable".format(server), False
-        except ldap.LDAPError as e:
-            if type(e.message) == dict and e.message.has_key('desc'):
-                print("Other LDAP error: {}".format(e.message['desc']))
-                return "Other LDAP error: {}".format(e.message['desc']), False
-            else:
-                print("Other LDAP error: {}".format(e))
-                return "Other LDAP error: {}".format(e), False
-        return conn, result
+        self.conn = Connection(self.AD_SERVERS[0],
+                               user=self.AD_BIND_USER,
+                               password=self.AD_BIND_PWD,
+                               auto_bind=True,
+                               auto_range=True)
 
-    def get_dn_by_username(self, username, ad_conn, basednx):
-        """
-        Get the Distinguished Name of a given user
-        :param username: Username of the account to search
-        :param ad_conn: Active Directory connection object
-        :param basedn: Base DN to search
-        :return: return_dn
-        """
-        basedn = self.AD_USER_BASEDN
-        return_dn = ''
-        ad_filter = self.AD_USER_FILTER.replace('{username}', username)
-        results = ad_conn.search_s(basedn, ldap.SCOPE_SUBTREE, ad_filter)
-        if results:
-            for dn, others in results:
-                return_dn = dn
-        return return_dn
 
-    def get_attr_by_dn(self, dn, ad_conn, attr=None):
-        """
-        Get attributes of a given user in Active Directory. If no attributes are specified,
-        a dictionary of all attributes will be returned
-
-        We'll only be querying enabled users with the following filter: (!(userAccountControl:1.2.840.113556.1.4.803:=2))
-        :param dn: Distinguished Name
-        :param ad_conn: Active Directory Connection object
-        :param attr: Optional - the attribute to return
-        :return: result
-        :type dn: str
-        :type attr: str
-        """
-        result = ad_conn.search_s(dn, ldap.SCOPE_BASE,
-                                  '(&(objectCategory=person)(objectClass=user)\
-                                  (!(userAccountControl:1.2.840.113556.1.4.803:=2)))')
-        d = 0
-        if attr is not None:
-            if result:
-                for dn, attrb in result:
-                    if attr in attrb and attrb[attr]:
-                        d = attrb[attr][0].lower()
-                        break
-            return d
-        else:
-            return result
-
-    def get_group_members(self, group_name, ad_conn):
+    def get_group_members(self, group_name):
         """
         Get members of the requested group in Active Directory
         :param group_name: The name of the group
-        :param ad_conn: Active Directory Connection object
-        :param basedn: Base DN for searching
         :return: members
         :rtype: list
         """
-        basedn = self.AD_GROUP_BASEDN
-        members = []
-        ad_filter = self.AD_GROUP_FILTER.replace('{group_name}', group_name)
-        result = ad_conn.search_s(basedn, ldap.SCOPE_SUBTREE, ad_filter)
-        if result:
-            if len(result[0]) >= 2 and 'member' in result[0][1]:
-                members_tmp = [u for u in [i.decode('utf-8') for i in result[0][1]['member']] if u != 0]
-                for m in members_tmp:
-                    try:
-                        attr = self.get_attr_by_dn(m, ad_conn, attr='sAMAccountName')
-                        if attr != 0:
-                            members.append(attr.decode('utf-8'))
-                    except Exception as exc:
-                        print(exc)
-        return members
+        member_list = []
+        entries = self.conn.extend.standard.paged_search(search_base=self.AD_BASEDN,
+                                                         search_filter=self.AD_GROUP_FILTER.replace('{group_name}', group_name),
+                                                         attributes=['member'],
+                                                         paged_size=self.AD_PAGE_SIZE)
+        for entry in entries:
+            if entry['type'] == 'searchResEntry':
+                for member in entry['attributes']['member']:
+                    member_list.append(self.get_attr_by_dn(member))
+        return member_list
+
+    def get_attr_by_dn(self, dn):
+        self.conn.search(search_base=self.AD_USER_BASEDN,
+                         search_filter=self.AD_USER_FILTER2.replace('{userdn}', dn),
+                         attributes=['sAMAccountName'])
+        username = self.conn.entries[0]['sAMAccountName']
+        return username
 
 
 def main():
@@ -170,15 +97,11 @@ def main():
     adsync = ADSync(settings_file)
     # Get AD users
     if args.ad_group:
-        print(args.ad_group)
-        # Make the Active Directory connection
-        ad_conn, result = adsync.ad_auth()
         # Get a list of users in the group
         ad_members = []
-        if result:
-            group_members = adsync.get_group_members(args.ad_group, ad_conn)
-            for m in group_members:
-                ad_members.append(m)
+        group_members = adsync.get_group_members(args.ad_group)
+        for m in group_members:
+            ad_members.append(m)
     # If we specify a GitHub Team (-t, --team)
     if args.team:
         if not args.org:
