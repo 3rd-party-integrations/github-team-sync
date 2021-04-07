8863d834f59d57c46d8f182f12a1f01f9ac8cf84
Buhleva <kabu@softwareag.com>
2021-04-07 10:58:27 +0300

    Reformat

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def sync_new_team():
     owner = github_app.payload["organization"]["login"]
     team_id = github_app.payload["team"]["id"]
     if os.environ["USER_DIRECTORY"].upper() == "AAD":
-        ## Azure APIs don't currently support case insensitive searching
+        # Azure APIs don't currently support case insensitive searching
         slug = github_app.payload["team"]["name"].replace(" ", "-")
     else:
         slug = github_app.payload["team"]["slug"]
@@ -132,7 +132,8 @@ def github_team_members(client=None, owner=None, team_id=None, attribute="userna
             )
     else:
         for member in team.members():
-            team_members.append({"username": str(member).casefold(), "email": ""})
+            team_members.append(
+                {"username": str(member).casefold(), "email": ""})
     return team_members
 
 
@@ -166,7 +167,8 @@ def execute_sync(org, team, slug, state):
     :param state:
     :return:
     """
-    total_changes = len(state["action"]["remove"]) + len(state["action"]["add"])
+    total_changes = len(state["action"]["remove"]) + \
+        len(state["action"]["add"])
     if len(state["directory"]) == 0:
         message = f"{os.environ.get('USER_DIRECTORY', 'LDAP').upper()} group returned empty: {slug}"
         raise ValueError(message)
@@ -237,11 +239,11 @@ def get_app_installations():
     """
     with app.app_context() as ctx:
         try:
-          c = ctx.push()
-          gh = GitHubApp(c)
-          installations = gh.app_client.app_installations
+            c = ctx.push()
+            gh = GitHubApp(c)
+            installations = gh.app_client.app_installations
         finally:
-          ctx.pop()
+            ctx.pop()
     return installations
 
 
