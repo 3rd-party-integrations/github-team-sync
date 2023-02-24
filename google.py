6d6b7f691216e720ed2083577b4b1710b38226e4
Dmitry Chepurovskiy <d.chepurovskiy@1inch.io>
2023-02-24 21:10:01 +0200

    WIP

---
+++ google.py
@@ -0,0 +1,36 @@
+from google.oauth2 import service_account
+import os
+import googleapiclient.discovery
+
+SCOPES = [
+   'https://www.googleapis.com/auth/admin.directory.group.readonly',
+   'https://www.googleapis.com/auth/admin.directory.group.member.readonly',
+   'https://www.googleapis.com/auth/admin.directory.user.readonly'
+   ]
+SERVICE_ACCOUNT_FILE = '/tmp/credentials.json'
+
+def create_service():
+  credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+  delegated_credentials = credentials.with_subject(os.environ.get("ADMIN"))
+
+  service = googleapiclient.discovery.build(
+    'admin',
+    'directory_v1',
+    credentials=delegated_credentials)
+
+  return service
+
+def main():
+    """Shows basic usage of the Admin SDK Directory API.
+    Prints the emails and names of the first 10 users in the domain.
+    """
+    service = create_service()
+    users = service.users().list(customer="my_customer", viewType="admin_view", projection="custom", customFieldMask="GithubUsername").execute().get('users', [])
+    for u in users:
+       githubUsername = u.get('customSchemas', {}).get("GithubUsername", {}).get("GithubUsername")
+       if githubUsername:
+          print(f"{u['primaryEmail']}: {githubUsername}")
+
+if __name__ == '__main__':
+    main()
+# [END admin_sdk_directory_quickstart]
